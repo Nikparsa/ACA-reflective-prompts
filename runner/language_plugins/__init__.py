b5901876b9400f0b20a3a2ca876c939e6e6f8583
Paria Nikparsa <paria.nikparsa@edu.fh-joanneum.at>
2026-07-14 12:12:44 +0200

    delete

--- runner/language_plugins/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Language plugins package."""
-
-from .plugin_manager import plugin_manager
-from .base_plugin import LanguagePlugin
-from .python_plugin import PythonPlugin
-
-__all__ = ['plugin_manager', 'LanguagePlugin', 'PythonPlugin']
